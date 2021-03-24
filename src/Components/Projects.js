5177974b9de0183102e60e7902bf543778e2f202
Manish Kumar <manish.hit2800@gmail.com>
2021-03-24 16:56:23 +0530

    project-added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -46,6 +46,17 @@ const Projects = () => {
           </header>
           <ul className="list-non-bullet">
           <li>
+                <article class="section ow showcase-list container container-center">
+                 <h1><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Arrow_east.svg/1280px-Arrow_east.svg.png" alt="" class="arrow"/>  RandomUserApp</h1>
+                 <small>March, 2021</small>
+                 <p>This is a React <strong>RandomUserApp</strong> that I've made using some <strong>npm dependencies</strong> like <strong>Reactstrap, bootstrap, React-icons, uuid, and npm package axios for fetching API</strong> and some CSS. I implemented this on <strong>codesandbox.io</strong>.</p>
+     
+                 <a class="link link-primary" href="https://zbzlm.csb.app/">Live Project</a>
+                 <a class="link link-secondary" href="https://github.com/manishk-99/RandomUser">View Source</a>
+                </article>
+             </li>
+             <br/>
+          <li>
                 <article class="section showcase-list container container-center">
                  <h1><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Arrow_east.svg/1280px-Arrow_east.svg.png" alt="" class="arrow"/>  localStorageTODO</h1>
                  <small>March, 2021</small>
